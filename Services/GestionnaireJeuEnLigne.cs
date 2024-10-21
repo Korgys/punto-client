2706558291a2729703c1e589400c58314bf813e9
Jonathan Eritzian <j0.eritzian@hotmail.fr>
2024-10-21 15:13:52 +0200

    Ajoute le socket pour afficher le plateau

--- Services/GestionnaireJeuEnLigne.cs
+++ Services/GestionnaireJeuEnLigne.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using punto_client.Models;
 
 namespace punto_client.Services;
 
@@ -32,6 +34,18 @@ public class GestionnaireJeuEnLigne
         _connection.On<List<int>>("MettreAJourTuilesEnMain", (tuilesEnMain) =>
         {
             Console.WriteLine("Vos tuiles en main : " + string.Join(", ", tuilesEnMain));
+        });
+
+        _connection.On<string>("MettreAJourPlateau", (jsonPlateau) =>
+        {
+            // Désérialise directement le JSON en une liste de tuiles
+            List<Tuile> tuiles = JsonConvert.DeserializeObject<List<Tuile>>(jsonPlateau);
+
+            // Crée un plateau et y assigne les tuiles
+            Plateau plateau = new Plateau { TuilesPlacees = tuiles };
+
+            // Affiche le plateau mis à jour
+            AfficherPlateau(plateau);
         });
 
         _connection.On<string>("TerminerJeu", (vainqueur) =>
@@ -80,6 +94,90 @@ public class GestionnaireJeuEnLigne
     public async Task Deconnecter()
     {
         await _connection.StopAsync();
+    }
+
+    /// <summary>
+    /// Affiche le plateau en console.
+    /// </summary>
+    public void AfficherPlateau(Plateau plateau)
+    {
+        int tailleGrille = 6 * 2 - 1; // 6 de rayon sur une grille de 11 de diametre
+        var grille = new string[tailleGrille, tailleGrille];
+        var tuilesPlacees = plateau.TuilesPlacees;
+
+        // Initialise la grille avec des "." pour les emplacements vides
+        for (int i = 0; i < tailleGrille; i++)
+        {
+            for (int j = 0; j < tailleGrille; j++)
+            {
+                grille[i, j] = " ";
+            }
+        }
+
+        // Remplit la grille avec les tuiles placées (sans affichage de couleur ici)
+        foreach (var tuile in tuilesPlacees)
+        {
+            if (tuile.PositionX > -6 && tuile.PositionX < 6 && tuile.PositionY > -6 && tuile.PositionY < 6)
+            {
+                grille[tuile.PositionY + 5, tuile.PositionX + 5] = tuile.Valeur.ToString();
+            }
+        }
+
+        // Affiche la grille dans la console avec des couleurs
+        Console.WriteLine("Plateau :");
+        Console.WriteLine(" X 5 4 3 2 1 0 1 2 3 4 5 6");
+        Console.WriteLine("  ________________________");
+        for (int i = 0; i < tailleGrille; i++)
+        {
+            if (i == 0) Console.Write(" Y|");
+            else if (i >= 5) Console.Write($" {i - 5}|"); // Affichage des nombres positifs avec un espace devant
+            else Console.Write($"{i - 5}|");
+
+            for (int j = 0; j < tailleGrille; j++)
+            {
+                // Cherche la tuile à cette position
+                var tuile = tuilesPlacees.FirstOrDefault(t => t.PositionX == j - 5 && t.PositionY == i - 5);
+                if (tuile != null)
+                {
+                    // Change la couleur en fonction du joueur
+                    AfficherMessageDeJoueur(tuile.Proprietaire.OrdreDeJeu, grille[i, j] + " ");
+                }
+                else
+                {
+                    // Affiche les emplacements vides avec la couleur par défaut
+                    Console.Write(grille[i, j] + " ");
+                }
+            }
+            Console.WriteLine(); // Retour à la ligne après chaque rangée
+        }
+
+        // Réinitialise la couleur à la fin
+        Console.ResetColor();
+    }
+
+    /// <summary>
+    /// Affiche un message en couleur dans la console en fonction de l'index du joueur.
+    /// </summary>
+    /// <param name="numeroJoueur"></param>
+    /// <param name="message"></param>
+    public static void AfficherMessageDeJoueur(int numeroJoueur, string message)
+    {
+        Console.ForegroundColor = numeroJoueur switch
+        {
+            1 => ConsoleColor.Magenta,// Joueur 1 - Magenta
+            2 => ConsoleColor.Cyan,// Joueur 2 - Cyan
+            3 => ConsoleColor.Green,// Joueur 3 - Vert
+            4 => ConsoleColor.Yellow,// Joueur 4 - Jaune
+            5 => ConsoleColor.Gray,// Joueur 5 - Gris
+            6 => ConsoleColor.Red,// Joueur 6 - Rouge
+            7 => ConsoleColor.DarkYellow,// Joueur 7 - Jaune foncé
+            8 => ConsoleColor.DarkRed,// Joueur 8 - rouge foncé
+            9 => ConsoleColor.DarkGray,// Joueur 9 - gris foncé
+            10 => ConsoleColor.DarkCyan,// Joueur 10 - cyan foncé
+            _ => ConsoleColor.White,// Autres joueurs ou cas par défaut
+        };
+        Console.Write(message);
+        Console.ResetColor();
     }
 }
 
